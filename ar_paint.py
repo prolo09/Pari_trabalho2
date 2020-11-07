def2be5bb0870b2d3f831ddefe520fe9cb764d87
Pedro Rolo <pmrolo@ua.pt>
2020-11-07 21:22:35 +0000

    por a escrever na tela Branca (sem modo especial)

--- ar_paint.py
+++ ar_paint.py
@@ -6,6 +6,10 @@ import cv2
 import json
 import numpy as np
 
+# variaveis globais
+ponto_ini=[0,0]
+tela = np.ones([500, 500, 3], 'uint8') * 255
+
 def escolhamodo():
     parser = argparse.ArgumentParser(description="PARI AR Paint")
     parser.add_argument('-js' , '--json' , help = "Full path to json file" , action="store_true")
@@ -33,21 +37,66 @@ def main():
         instrucoes()
 
     elif escolha['json']:
-        #limites = json.load(open("limits.json"))
+        limites = json.load(open("limits.json"))
+
 
         cap = cv2.VideoCapture(0)
         name= 'AR_Paint'
         cv2.namedWindow(name, cv2.WINDOW_AUTOSIZE)
 
+
+        text='Azul'
         while True:
             #c = readchar.readkey()
             _,frame = cap.read()
+
+
+            # vai buscar os valore dos limites
+            lim_inf=np.array([int(limites['B']['min']),int(limites['G']['min']),int(limites['R']['min'])])
+            lim_sup=np.array([int(limites['B']['max']), int(limites['G']['max']), int(limites['R']['max'])])
+
+            # aplica o limites e cria uma mascara
+            mask = cv2.inRange(frame, lim_inf, lim_sup)
+
+
+            # encotra o bloco maior
+
+            _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            if len(contours) !=0:
+                global tela, ponto_ini
+                c = max(contours, key=cv2.contourArea)
+                x, y, w, h = cv2.boundingRect(c)
+
+                X_cm=x+w/2
+                Y_cm=y+h/2
+
+
+                #cv2.circle(frame,(X_cm,Y_cm),10,(0,255,0), -1)
+
+                cv2.line(tela, (X_cm, Y_cm), (ponto_ini[0],ponto_ini[1]) ,(255,0,0),10)
+                ponto_ini=(X_cm,Y_cm)
+
+
+
+
+            cv2.imshow('tela',tela)
+            cv2.putText(frame,text,(40,40),1,3,(255,255,255))
             cv2.imshow(name,frame)
+            cv2.imshow('ff', mask)
             c = cv2.waitKey(1)
+
+
+
+
+
+
+
 
             # Criar mask onde se pinta
 
             if c == 114:        # Prime 'r' para riscar red
+                text='red'
                 print ('red')
                 del c
 
