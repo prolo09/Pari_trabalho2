01a126640623e45bed37e8d55aa43ac89059ae95
alexdannyvaladares <alexdannyv@hotmail.com>
2020-11-08 11:03:36 +0000

    So alguns comentarios e restruturação de algumas variaveis

--- ar_paint.py
+++ ar_paint.py
@@ -7,28 +7,39 @@ import json
 import numpy as np
 
 # variaveis globais
-ponto_ini=[0,0]
+ponto_ini = [0, 0]
 tela = np.ones([500, 500, 3], 'uint8') * 255
+
 
 def escolhamodo():
     parser = argparse.ArgumentParser(description="PARI AR Paint")
-    parser.add_argument('-js' , '--json' , help = "Full path to json file" , action="store_true")
-    parser.add_argument('-info' , '--more_info' , help = "List of commands", action="store_true")
+    parser.add_argument('-js',
+                        '--json',
+                        help="Full path to json file",
+                        action="store_true")
+    parser.add_argument('-info',
+                        '--more_info',
+                        help="List of commands",
+                        action="store_true")
     args = vars(parser.parse_args())
     return args
 
+
 def instrucoes():
-    # lista de instrucoes
-    print("!! PARI AR Paint !!")
-    print("Here's some commands:")
-    print("- QUIT          -> PRESS 'q'")
-    print("- CLEAR         -> PRESS 'c'")
-    print("- SAVE          -> PRESS 'w'")
-    print("- RED PAINT     -> PRESS 'r'")
-    print("- GREEN PAINT   -> PRESS 'g'")
-    print("- BLUE PAINT    -> PRESS 'b'")
-    print("- THICKER BRUSH -> PRESS '+'")
-    print("- THINNER BRUSH -> PRESS '-'")
+    # Command list
+    print('''
+    !! PARI AR PAINT !!
+    Here are some commands u can use:
+    - RED PAINT     -> PRESS 'r'
+    - GREEN PAINT   -> PRESS 'g'
+    - BLUE PAINT    -> PRESS 'b'
+    - THICKER BRUSH -> PRESS '+'
+    - THINNER BRUSH -> PRESS '-'
+    - SAVE          -> PRESS 'w'
+    - CLEAR         -> PRESS 'c'
+    - QUIT          -> PRESS 'q'          
+    ''')
+
 
 def main():
     escolha = escolhamodo()
@@ -39,98 +50,81 @@ def main():
     elif escolha['json']:
         limites = json.load(open("limits.json"))
 
-
         cap = cv2.VideoCapture(0)
-        name= 'AR_Paint'
+        name = 'AR_Paint'
         cv2.namedWindow(name, cv2.WINDOW_AUTOSIZE)
 
-
-        text='Azul'
+        text = 'Azul'
         while True:
-            #c = readchar.readkey()
-            _,frame = cap.read()
-
+            # c = readchar.readkey()
+            _, frame = cap.read()
 
             # vai buscar os valore dos limites
-            lim_inf=np.array([int(limites['B']['min']),int(limites['G']['min']),int(limites['R']['min'])])
-            lim_sup=np.array([int(limites['B']['max']), int(limites['G']['max']), int(limites['R']['max'])])
+            lim_inf = np.array([int(limites['B']['min']), int(limites['G']['min']), int(limites['R']['min'])])
+            lim_sup = np.array([int(limites['B']['max']), int(limites['G']['max']), int(limites['R']['max'])])
 
             # aplica o limites e cria uma mascara
             mask = cv2.inRange(frame, lim_inf, lim_sup)
-
 
             # encotra o bloco maior
 
             _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            if len(contours) !=0:
+            if len(contours) != 0:
                 global tela, ponto_ini
                 c = max(contours, key=cv2.contourArea)
                 x, y, w, h = cv2.boundingRect(c)
 
-                X_cm=x+w/2
-                Y_cm=y+h/2
+                X_cm = x + w / 2
+                Y_cm = y + h / 2
 
+                # cv2.circle(frame,(X_cm,Y_cm),10,(0,255,0), -1)
 
-                #cv2.circle(frame,(X_cm,Y_cm),10,(0,255,0), -1)
+                cv2.line(tela, (X_cm, Y_cm), (ponto_ini[0], ponto_ini[1]), (255, 0, 0), 10)
+                ponto_ini = (X_cm, Y_cm)
 
-                cv2.line(tela, (X_cm, Y_cm), (ponto_ini[0],ponto_ini[1]) ,(255,0,0),10)
-                ponto_ini=(X_cm,Y_cm)
-
-
-
-
-            cv2.imshow('tela',tela)
-            cv2.putText(frame,text,(40,40),1,3,(255,255,255))
-            cv2.imshow(name,frame)
+            cv2.imshow('tela', tela)
+            cv2.putText(frame, text, (40, 40), 1, 3, (255, 255, 255))
+            cv2.imshow(name, frame)
             cv2.imshow('ff', mask)
             c = cv2.waitKey(1)
 
-
-
-
-
-
-
-
             # Criar mask onde se pinta
 
-            if c == 114:        # Prime 'r' para riscar red
-                text='red'
+            if c == 114:  # Prime 'r' para riscar red
+                text = 'red'
                 print ('red')
                 del c
 
-            elif c == 103:      # Prime 'g' para riscar green
+            elif c == 103:  # Prime 'g' para riscar green
                 print ('green')
                 del c
 
-            elif c == 98:       # Prime 'b' para riscar blue
+            elif c == 98:  # Prime 'b' para riscar blue
                 print ('blue')
                 del c
 
-            elif c == 43:       # Prime '+' para aumentar largura de risco
+            elif c == 43:  # Prime '+' para aumentar largura de risco
                 print ('larger')
                 del c
 
-            elif c == 45:       # Prime '-' para diminuir largura de risco
+            elif c == 45:  # Prime '-' para diminuir largura de risco
                 print ('thinner')
                 del c
 
-            elif c == 99:       # Prime 'c' para limpar a tela
+            elif c == 99:  # Prime 'c' para limpar a tela
                 print ('clear')
                 del c
 
-            elif c == 119:       # Prime 'w' para guardar sketch
+            elif c == 119:  # Prime 'w' para guardar sketch
                 print ('save')
                 del c
 
-            elif c == 113:       # Prime 'q' para sair
+            elif c == 113:  # Prime 'q' para sair
                 cv2.destroyAllWindows()
                 break
     else:
         instrucoes()
-
-
 
 
 if __name__ == '__main__':
--- color_segmenter.py
+++ color_segmenter.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python
-
+# --------------------------------------------------
+# A  python script to segment a videocapture to isolate a color.Created by :
+# Alex Valadares,84786
+# Pedro Rolo,
+# Pedro Tavares,
+# PARI, November 2020.
+# --------------------------------------------------
 import cv2
 import argparse
 import json
 import numpy as np
 
 
-
 def escolhamodo():
-    # funcao para escolher os modos
+    # Function to choose between color and show color segmentation mode
     esc_modo = argparse.ArgumentParser(description="escolhe modo de canis")
-    esc_modo.add_argument('-hsv', help="moda para o modo hvs", action="store_true")
-    esc_modo.add_argument('--mostraCor', help="mostra a cor isolada", action="store_true")
-    arg_list=vars(esc_modo.parse_args())
+    esc_modo.add_argument('-hsv',
+                          help="moda para o modo hvs", action="store_true")
+    esc_modo.add_argument('--mostraCor',
+                          help="mostra a cor isolada",
+                          action="store_true")
+    arg_list = vars(esc_modo.parse_args())
     return arg_list
 
 
 def nothing(x):
+    # Function that just passes without doing nothing, used dor createTrackbar.
     pass
 
+
 def main():
+    escolha = escolhamodo()
 
-    escolha=escolhamodo()
-
-
-    windon_name='segmentacao'
-
+    windon_name = 'segmentacao'
     cap = cv2.VideoCapture(0)
     cv2.namedWindow(windon_name, cv2.WINDOW_AUTOSIZE)
-
-
-
-
 
     # criar as trackbar para escolha dos valores
     cv2.createTrackbar('min B/H', windon_name, 0, 255, nothing)
@@ -41,34 +44,27 @@ def main():
     cv2.createTrackbar('min R/V', windon_name, 0, 255, nothing)
     cv2.createTrackbar('max R/V', windon_name, 0, 255, nothing)
 
-
-
     while True:
 
         _, frame = cap.read()
 
-        #grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         if escolha["hsv"]:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-
-        # vou buscar o valor defenidos nas trackbar
-        minB_H=cv2.getTrackbarPos('min B/H', windon_name)
+        # Gets Trackbars values
+        minB_H = cv2.getTrackbarPos('min B/H', windon_name)
         maxB_H = cv2.getTrackbarPos('max B/H', windon_name)
         minG_S = cv2.getTrackbarPos('min G/S', windon_name)
         maxG_S = cv2.getTrackbarPos('max G/S', windon_name)
         minR_V = cv2.getTrackbarPos('min R/V', windon_name)
         maxR_V = cv2.getTrackbarPos('max R/V', windon_name)
 
-
-        # cria matriz com os limites sinferior e superiors
-        lim_inf= np.array([minB_H,minG_S,minR_V])
-        lim_sup=np.array([maxB_H,maxG_S,maxR_V])
-
+        # MATRIX with lower and upper limtis
+        lim_inf = np.array([minB_H, minG_S, minR_V])
+        lim_sup = np.array([maxB_H, maxG_S, maxR_V])
 
         # aplica um treshold com esse limites
-        mask=cv2.inRange(frame, lim_inf, lim_sup)
+        mask = cv2.inRange(frame, lim_inf, lim_sup)
 
         if escolha["mostraCor"]:
             # caso quera imprimir nao mostrando a cor mesmo real
@@ -80,17 +76,13 @@ def main():
             # para imprimir caso queira isolar a cor e mostrando
             cv2.imshow(windon_name, mask)
 
-
-        key=cv2.waitKey(1)
-        if key==113:
+        key = cv2.waitKey(1)
+        if key == 113:
             break
-        elif key==119:
+        elif key == 119:
             dic_limite = dict(B={'max': maxB_H, 'min': minB_H},
                               G={'max': maxG_S, 'min': minG_S},
                               R={'max': maxR_V, 'min': minR_V})
-
-
-
 
             file_name = 'limits.json'
             with open(file_name, 'w') as file_handle:
@@ -101,6 +93,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
